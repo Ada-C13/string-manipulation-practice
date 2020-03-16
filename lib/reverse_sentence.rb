aae2612549848871e2bb4f62a233599b18b104af
Olga Patrakova <olgapatrakova@outlook.com>
2020-03-15 17:50:04 -0700

    Reverse_sentence complete

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,33 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence == nil
+  # reverse the whole string
+  new_sentence = ''
+  my_sentence.length.times do |i|
+    element = my_sentence[i]
+    new_sentence[i] = my_sentence[-(i+1)]
+    new_sentence[-(i+1)] = element
+  end
+
+  i = 0
+  word = ''
+  result = ''
+  while i < new_sentence.length
+    if new_sentence[i] != ' '
+      word = new_sentence[i] + word
+    else
+      result = result + word
+      word = ''
+      result = result + ' '
+    end
+    i += 1
+  end
+  result = result + word
+
+  result.length.times do |i|
+    my_sentence[i] = result[i]
+  end
+  return true
 end
