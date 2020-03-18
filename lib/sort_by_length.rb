fa85fb5a9af7b7bd591af73e5ae5cdfa42024902
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-18 14:12:14 -0700

    removed unnecessary incrementer

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -12,25 +12,18 @@ def sort_by_length(my_sentence)
     my_sentence_array = my_sentence.split
     # save length of array to varialbe
     my_sentence_array_length = my_sentence_array.length
-    puts "this is the length"
-    puts my_sentence_array_length
-    # start loop
+    # start loop (I tried 'while' and 'until' but could not get either to work??)
       loop do  
         # set swapped to false
         swapped = false
         # create times loop and go over each index in array until (length - 1)
         (my_sentence_array_length - 1).times do |index|
-          puts "inside while loop"
-          index = 0
           # check if index[0].length is greater than index[1].length
           if my_sentence_array[index].length > my_sentence_array[index + 1].length
-            puts "inside if statement"
             # if condition above is true...swap those two indexes with each other - this is the swapping
               my_sentence_array[index], my_sentence_array[index + 1] = my_sentence_array[index + 1], my_sentence_array[index]
               # change swapped value to true so that the loop continues
               swapped = true
-              puts "this should be true"
-              puts swapped
           end
         end
         # if no swaps, then input is in order and break out of loop
