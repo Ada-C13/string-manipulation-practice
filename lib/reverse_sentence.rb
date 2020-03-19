4e8bb11ad0614ff80d0d8f510bc6121ce79e3535
Katie <katie.vandervoort@gmail.com>
2020-03-19 13:21:42 -0700

    completed waves 1 and 2

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,31 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence == nil
+  cloned_sentence = my_sentence.clone
+  index_from_begining = 0
+  word = ""
+  cloned_sentence.length.times do |i|
+    index = -1 - i
+    if cloned_sentence[index] == " "
+      my_sentence[index_from_begining] = cloned_sentence[index]
+      index_from_begining += 1
+    elsif cloned_sentence[index - 1] != " "
+      word.insert(0,cloned_sentence[index])
+    else
+      word.insert(0,cloned_sentence[index])
+      word.length.times do |x|
+        my_sentence[index_from_begining] = word[x]
+        index_from_begining += 1
+      end
+      word = ""
+    end
+  end
+
+  word.length.times do |x|
+    my_sentence[index_from_begining] = word[x]
+    index_from_begining += 1
+  end
+  return my_sentence
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,28 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array_of_words = my_sentence.split
+  sorted_array = []
+  array_of_words.each_with_index do |word, index_on_normal_array|
+    if index_on_normal_array == 0
+      sorted_array.push(array_of_words[0])
+    else
+      sorted_array_clone = sorted_array.clone
+      sorted_array_clone.each_with_index do |word_in_sorted_array,index_in_sorted_array|    
+        if word.length < word_in_sorted_array.length
+          sorted_array.insert(index_in_sorted_array,word)
+        elsif word.length == word_in_sorted_array.length && word.length < word_in_sorted_array.length
+          sorted_array.insert(sorted_array_clone.length,word) 
+        elsif word.length >= word_in_sorted_array.length && index_in_sorted_array == (sorted_array_clone.length - 1)
+          sorted_array.insert(sorted_array_clone.length,word)      
+        end
+
+      end
+
+    end
+
+  end
+  return sorted_array
 end
