c7601b721d58206d2b840c9fa6fb1b3cc690198f
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-19 15:25:24 -0700

    completed wave 1 and passed the test!

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,31 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return [] if my_sentence == ""
+  
+  #Convert the string to an array of strings
+  strings_array = my_sentence.split(/ /)
+  
+  # Sort (use selection)the length of each element 
+  i = 0 
+  while i < strings_array.length - 1
+    min_index = i 
+    j = i+1
+    while j < strings_array.length
+      if strings_array[j].length < strings_array[min_index].length
+        min_index = j
+      end
+      j +=1
+    end
+    if min_index != i
+      temp = strings_array[min_index]
+      strings_array[min_index] = strings_array[i]
+      strings_array[i] = temp
+      return strings_array
+    end
+    i +=1
+  end
 end
+
+
+
