4584aac868f7733718368dee4502ba5330b50c1c
Halahaddad1 <h.m.haddad@gmail.com>
2020-03-19 02:39:58 -0700

    all tests passing

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'http://rubygems.org'
 
-ruby '2.5.1'
 
 gem 'rake'
 
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,23 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(N)
+# Space complexity: O(N)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  if my_sentence == ""
+    return []
+  else 
+    my_arr = my_sentence.split
+    sorted_arr = []
+    index = 0
+    sorted_arr<< my_arr.shift
+    my_arr.each do |word|
+      if word.length < sorted_arr[index].length 
+        sorted_arr.insert(index,word)
+      elsif index+1 == sorted_arr.size
+        sorted_arr<< word
+      end
+      index +=1
+    end
+  end
+  return sorted_arr
 end
