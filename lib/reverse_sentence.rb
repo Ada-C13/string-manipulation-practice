c9232aca8cb2c7b73351f6326dbfb13ce5e431cb
Corinna Fabre <corinna.fabre@gmail.com>
2020-03-20 09:51:55 -0700

    FINALLY reverse_sentence works AND I understand it.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,22 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)? because we are doing two loops but we drop the constants
+# Space complexity: O(1) -- I don't feel confident about this because of the creation of split_sentence and/or reversed
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence == nil
+  
+  split_sentence = my_sentence.scan(/\S+|\s+/)
+  reversed = ""
+
+  i = 0
+
+  while i < split_sentence.length do
+    reversed += split_sentence[-(i+1)]
+    i += 1
+  end
+
+  reversed.length.times do |n|
+    my_sentence[n] = reversed[n]
+  end
+
+  return my_sentence
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,20 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(1)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
-end
+  split_sentence = my_sentence.split(" ")  
+      #adapted from Insertion Sort lecture
+        i = 1
+        while i < split_sentence.length
+          to_insert = split_sentence[i]
+          j = i
+          while j > 0 && split_sentence[j-1].length > to_insert.length
+            split_sentence[j] = split_sentence[j-1]
+            j -= 1
+          end
+          split_sentence[j] = to_insert
+          i += 1
+        end
+        p split_sentence
+      end
