8f09e2911280e5dbdba7e829ad432ee7f1b9e40c
Rosalyn <syrosalynyu@gmail.com>
2020-03-19 18:20:11 -0700

    completed rever sentence. But instead of modifying the original sentence, my solution return a new string. Therefore, I had to modify all the test cases(use expect instead) in order to pass the tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require_relative '../lib/reverse_sentence'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative "../lib/reverse_sentence"
+require_relative "../lib/reverse_sentence_option_2"
+require_relative "../lib/reverse_sentence_option_1"
 require_relative "../lib/sort_by_length"
 
