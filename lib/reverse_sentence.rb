2c088bcef01ad83cd4d407e062137c54483fed7c
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-19 20:05:02 -0700

    Started reverse sentence, reserves words but excludes spaces

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,21 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
+  return my_sentence unless my_sentence.class == String
   
+  my_sentence = my_sentence.split(" ")
+
+  my_length = my_sentence.length
+  reserved_sentence = []
+
+  my_length.times do |i|
+    reserved_sentence << my_sentence.last
+    my_sentence.pop
+  end
+  my_sentence = ""
+
+  reserved_sentence.map do |word|
+    my_sentence += word
+  end
+  return my_sentence
 end
