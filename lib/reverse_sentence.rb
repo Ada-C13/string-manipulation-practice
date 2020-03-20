0d30936ce8357b57732a011fb43e8833d48dc240
ChelseaC13 <chelhelg@outlook.com>
2020-03-19 20:38:57 -0700

    finished

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,27 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n) one each loop
+# Space complexity: O(n) made a reverse and a word holder
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence == ""
+    return ""
+  elsif my_sentence == nil
+    return nil
+  end
+
+  split = my_sentence.split("")
+  reverse = ""
+
+  word_holder = ""
+
+  split.each do |char|
+    if char == " "
+      reverse = "#{word_holder}#{reverse}"
+      reverse = "#{char}#{reverse}"
+      word_holder = ""
+    elsif char != " "
+      word_holder = word_holder + char
+    end
+  end
+  reverse = "#{word_holder}#{reverse}"
+  return reverse
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,31 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n^2) two nested while loops based on input
+# Space complexity: o(n) one array made (word_array)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  if my_sentence.length == 0
+    return []
+  end
+
+  word_array = my_sentence.split(" ")
+  length = word_array.length
+  i = 0
+  
+  while i < length - 1
+    min_index = i
+    j = i + 1
+    while j < length
+      if word_array[j].length < word_array[min_index].length
+        min_index = j
+      end
+      j += 1
+    end
+    if min_index != i
+      temp = word_array[min_index]
+      word_array[min_index] = word_array[i]
+      word_array[i] = temp
+    end
+    i += 1
+  end 
+  return word_array
 end
