b19a04251d660f110193aa41312446f3fc1b2e6e
thenora <thenora@gmail.com>
2020-03-20 14:37:14 -0700

    Sort by length done

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,22 +1,26 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: 0(n^2)
+# Space complexity: 0(n)
 def sort_by_length(my_sentence)
   array = my_sentence.split(" ")
   length = array.length
 
-  i = 1
+  i = 0
   while i < length
     to_insert = array[i]
-    j = i
+    j = 0
     # search in the sorted portion of the array
     # for the correct position to insert array[i]
-    while j > 0 && array[j - 1] > to_insert
-      array[j] = array[j - 1]
-      j -= 1
+    while j < length - i - 1
+      if array[j].length > array[j + 1].length
+        temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      end
+
+      j += 1
     end
-    array[j] = to_insert
     i += 1
   end
 
