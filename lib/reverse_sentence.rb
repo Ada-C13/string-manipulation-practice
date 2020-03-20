2d20cb45fcfe8aef1c379a65ac4ec51ee083a55a
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-20 09:06:23 -0700

    adds comment about logic, changes method to reverse by char

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -4,17 +4,19 @@
 def reverse_sentence(my_sentence)
   return my_sentence if my_sentence.nil? || my_sentence.empty?
   
-  split_sentence = my_sentence.split(/[\s,]+/)
   first_index = 0
-  last_index = split_sentence.length - 1
+  last_index = my_sentence.length - 1
 
   while first_index < last_index
-    temp = split_sentence[first_index]
-    split_sentence[first_index] = split_sentence[last_index]
-    split_sentence[last_index] = temp
+    temp = my_sentence[first_index]
+    my_sentence[first_index] = my_sentence[last_index]
+    my_sentence[last_index] = temp
     first_index += 1
     last_index -= 1
   end
 
   return split_sentence
 end
+
+# Hello world => dlrow olleH => world Hello
+# reverse by char and reverse by word! 
