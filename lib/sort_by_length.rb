f7317b70194306788f236840053b921f757b7088
thenora <thenora@gmail.com>
2020-03-20 14:09:06 -0700

    setup for insertion

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split(" ")
+  length = array.length
+
+  i = 1
+  while i < length
+    to_insert = array[i]
+    j = i
+    # search in the sorted portion of the array
+    # for the correct position to insert array[i]
+    while j > 0 && array[j - 1] > to_insert
+      array[j] = array[j - 1]
+      j -= 1
+    end
+    array[j] = to_insert
+    i += 1
+  end
+
+  return array
 end
