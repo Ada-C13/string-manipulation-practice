3f5ae70d8a4f09670980e8e7a4fc99f9f55f5405
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-20 07:23:15 -0700

    reverses sentence by creating array, fails multiple word tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,19 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return my_sentence if my_sentence.nil? || my_sentence.empty?
+  
+  split_sentence = my_sentence.split(/[\s,]+/)
+  first_index = 0
+  last_index = split_sentence.length - 1
+
+  while first_index < last_index
+    temp = split_sentence[first_index]
+    split_sentence[first_index] = split_sentence[last_index]
+    split_sentence[last_index] = temp
+    first_index += 1
+    last_index -= 1
+  end
+
+  return split_sentence
 end
