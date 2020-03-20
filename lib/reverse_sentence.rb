613cb6b6bd07211c3883a2c30cd7c0a7476c846e
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-19 20:21:30 -0700

    Deleted unneeded comment

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -4,7 +4,7 @@
 def reverse_sentence(my_sentence)
   return "" if my_sentence == "" 
   return nil if my_sentence == nil
-  # return my_sentence if my_sentence.split.length == 1
+
   strings_array = my_sentence.split(/ /, -1)
 
   first_index = 0
