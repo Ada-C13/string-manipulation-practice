17102ff50f43f2d97f72a83e2401752a30e4e845
Stephanie <stephaniejmars@gmail.com>
2020-03-20 09:44:17 -0700

    project submission, reverse_sentence and sort_by_length

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,47 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+def reverse(array, length)
+  i = 0
+  j = length - 1
+
+  while i < j 
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    i += 1
+    j -= 1
+  end
+  return array
+end
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return if my_sentence == nil 
+  original_sentence = my_sentence.clone 
+  reversed_string = reverse(my_sentence, my_sentence.length)
+  
+  words = original_sentence.split(" ")
+  reversed_words = reverse(words, words.length)
+  new_sentence = ""
+
+  reversed_words.each do |words|
+    new_sentence << words
+  end
+
+  i = 0
+  reversed_string.length.times do 
+    if reversed_string[i] == " "
+      new_sentence.insert(i," ")
+    end
+    i += 1
+  end
+  
+  # diana helped me with this, I'm still not sure why it was necessary
+  new_sentence.length.times do |i|
+    my_sentence[i] = new_sentence[i]
+  end
+
+  return my_sentence
+
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,21 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
-end
+  #insertion sort 
+  array_words = my_sentence.split(" ")
+  
+  i = 1
+  while i < array_words.length 
+    j = i 
+    while j > 0 && array_words[j - 1].length > array_words[j].length
+      array_words[j - 1], array_words[j] = array_words[j],array_words[j - 1]
+      j -= 1
+    end
+    i += 1
+  end
+  
+  return array_words
+  end
+  
