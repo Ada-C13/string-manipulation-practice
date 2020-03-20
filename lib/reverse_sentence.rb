c67b79ca357c9d00f1477e535b5ad1172cdb7084
Jessica Stone <jstone4815@gmail.com>
2020-03-20 16:11:10 -0700

    refactor reverse_string and add time and space complexity explanations

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,8 +1,10 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2) - There are two outer loops, which is 2n, drop the 2, so n. Inside the second loop, there is a nested loop for each if statement, which makes ie O(n^2)
+# Space complexity: O(1) - the amount and size of variables is the same regardless of the input
 def reverse_sentence(my_sentence)
   return [] if my_sentence == nil
+
+  # reverse entire sentence
   first_char = my_sentence[0]
   (my_sentence.length/2).times do |i|
     my_sentence[i] = my_sentence[my_sentence.length - 1 - i]
@@ -11,57 +13,30 @@ def reverse_sentence(my_sentence)
   end
 
   first_index = 0
-  last_index = 0
   my_sentence.length.times do |i|
+    first_character = my_sentence[first_index]
+    # reverse the last word
     if i == my_sentence.length - 1
-      last_index = i
-      first_character = my_sentence[first_index]
-      if ((last_index - first_index) % 2) == 0
-        ((last_index - first_index) / 2).times do |x|
-          my_sentence[first_index] = my_sentence[last_index]
-          my_sentence[last_index] = first_character
-          first_index += 1
-          last_index -= 1
-          first_character = my_sentence[first_index]
-        end
-      else
-        (((last_index - first_index) / 2) + 1).times do |x|
-          my_sentence[first_index] = my_sentence[last_index]
-          my_sentence[last_index] = first_character
-          first_index += 1
-          last_index -= 1
-          first_character = my_sentence[first_index]
-        end
-      end
-    end
-    if my_sentence[i] == " "
-      check_next = 1
-      new_first_index = i + check_next
-      until my_sentence[i + check_next] != " "
-        check_next += 1
-      end
-      last_index = i - 1
-      first_character = my_sentence[first_index]
-      if ((last_index - first_index) % 2) == 0
-        ((last_index - first_index) / 2).times do |x|
-          my_sentence[first_index] = my_sentence[last_index]
-          my_sentence[last_index] = first_character
-          first_index += 1
-          last_index -= 1
-          first_character = my_sentence[first_index]
-        end
-      else
-        (((last_index - first_index) / 2) + 1).times do |x|
-          my_sentence[first_index] = my_sentence[last_index]
-          my_sentence[last_index] = first_character
-          first_index += 1
-          last_index -= 1
-          first_character = my_sentence[first_index]
-        end
-      end
-      first_index = i + check_next
+      reverse_word(i, first_index, first_character, my_sentence)
+    elsif my_sentence[i] == " "
+      reverse_word(i - 1, first_index, first_character, my_sentence)
+      # find new first_index
+      first_index = i
+      first_index += 1 until my_sentence[first_index] != " "
     end
   end
     
   return my_sentence
+end
+
+def reverse_word(last_index, first_index, first_character, my_sentence)
+  # add one to handle even and odd lengths
+  loops = (last_index - first_index) / 2 + 1
+  loops.times do |x|
+    my_sentence[first_index] = my_sentence[last_index]
+    my_sentence[last_index] = first_character
+    first_index += 1
+    last_index -= 1
+    first_character = my_sentence[first_index]
+  end
 end
