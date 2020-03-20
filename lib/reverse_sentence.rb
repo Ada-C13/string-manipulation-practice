7e28bb74e478b4a0a66b67ce075eed2e48cd0a37
cojenco <icathyouyang@gmail.com>
2020-03-19 19:45:08 -0700

    Add methods for reverse_sentence and insertion_sort

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,30 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if !my_sentence.is_a?(String)
+  my_sentence = reverse(my_sentence, 0, my_sentence.length - 1)
+  previous_index = 0
+
+  my_sentence.length.times do |i|
+    if /\s/ === my_sentence[i]
+      reverse(my_sentence, previous_index, i - 1)
+      previous_index = i + 1
+    end
+  end
+
+  return reverse(my_sentence, previous_index, my_sentence.length - 1)
+end
+
+
+def reverse(my_string, first_index, last_index)
+  while first_index < last_index
+    temp = my_string[first_index]
+    my_string[first_index] = my_string[last_index]
+    my_string[last_index] = temp
+    first_index += 1
+    last_index -= 1
+  end
+
+  return my_string
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,27 @@
 # A method which will return an array of the words in the string
-#  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+#  sorted by the length of the word.  (Using Insertion Sort)
+# Time complexity: O(n^2)
+# Space complexity: O(1)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  split_strings = my_sentence.split(" ")
+  i = 1
+
+  while i < split_strings.length
+    j = i
+
+    while j > 0
+      if split_strings[j].length < split_strings[(j-1)].length
+        temp = split_strings[j]
+        split_strings[j] = split_strings[j-1]
+        split_strings[j-1] = temp
+      else
+        break
+      end
+      j -= 1
+    end
+
+    i += 1
+  end
+
+  return split_strings
 end
