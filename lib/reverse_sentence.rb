8f4da164007fc6f3a708da9814b3c0283c6a61a6
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-20 13:15:29 -0700

    fixes error with finding the boundaries of words, reverses each word in place

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -3,8 +3,8 @@
 # Space complexity: ?
 def reverse_sentence(my_sentence)
   return my_sentence if my_sentence.nil? || my_sentence.empty?
-  
-  split_sentence = my_sentence.split(/[\s]/)
+
+  # reverse sentence by character
   first_index = 0
   last_index = my_sentence.length - 1
 
@@ -16,13 +16,29 @@ def reverse_sentence(my_sentence)
     last_index -= 1
   end
 
+  # identifies the boundaries of words by finding the spaces
   find_spaces = []
   my_sentence.length.times do |index|
-    find_space << index if /\s/.match(my_sentence[index])
+    find_spaces << index if my_sentence[index] == " "
+  end
+  find_spaces << my_sentence.length # add the index of the last space (i.e., the end of the sentence)
+  
+  # reverse each word
+  first_index = 0
+
+  find_spaces.each do |end_of_word|
+    last_index = end_of_word - 1
+
+    while first_index < last_index
+      temp = my_sentence[first_index]
+      my_sentence[first_index] = my_sentence[last_index]
+      my_sentence[last_index] = temp
+      first_index += 1
+      last_index -= 1
+    end
+    
+    first_index = end_of_word + 1
   end
   
   return my_sentence
 end
-
-# Hello world => dlrow olleH => world Hello
-# reverse by char and reverse by word! 
