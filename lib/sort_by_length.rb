d5497ef6ae34b696ace7448ed562f514967ea792
Diana Nguyen <41170709+dnguye2@users.noreply.github.com>
2020-03-19 18:40:31 -0700

    completed code

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,23 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  i = 0
+  sentence_array = my_sentence.split(" ")
+
+  while i < (sentence_array.length)-1
+    j = 0
+    while j < (sentence_array.length)-i-1
+      if (sentence_array[j].length) > (sentence_array[j+1].length)
+        temp = sentence_array[j]
+        sentence_array[j] = sentence_array[j+1]
+        sentence_array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return sentence_array
 end
