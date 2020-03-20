36b17b47d14a8b43bd6b3afc559995744211611a
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-19 21:04:23 -0700

    Pass all tests, which were altered to reflect that this method does not change in place

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -4,19 +4,41 @@
 def reverse_sentence(my_sentence)
   return my_sentence unless my_sentence.class == String
   
-  my_sentence = my_sentence.split(" ")
+  my_sentence = my_sentence.split("")
 
   my_length = my_sentence.length
-  reserved_sentence = []
+  reverse = []
+  my_regex = /[^ ]/
+  word = ""
+  space = ""
 
   my_length.times do |i|
-    reserved_sentence << my_sentence.last
-    my_sentence.pop
+    #make a word if it's not a space or char
+    if my_regex.match?(my_sentence[i])
+      reverse << space if (reverse.length == 0 || word.length == 0)
+      word += my_sentence[i]
+      space = ""
+    #make a space-word if it's a space
+    else
+      reverse << word if (reverse.length == 0 || space.length == 0)
+      space += " "
+      word = ""
+    end
   end
-  my_sentence = ""
+  #make sure the last word/space makes it into the array
+  reverse << word if reverse.last != word
+  reverse << space if reverse.last != space
 
-  reserved_sentence.map do |word|
-    my_sentence += word
+  #now reverse it!
+  an_array = []
+  reverse.length.times do |i|
+    an_array << reverse.pop
+  end
+
+  #now put it into a string!
+  my_sentence = ""
+  an_array.map do |my_word|
+    my_sentence += my_word
   end
   return my_sentence
 end
