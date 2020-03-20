7a1e1ce45448c80445c47bc66812bf6cc449f21d
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-20 15:39:22 -0700

    adds comments and pseudocode

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,6 +2,13 @@
 #  sorted by the length of the word.
 # Time complexity: ?
 # Space complexity: ?
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  # Insertion sort method because it performs best out of the three when working with small arrays, and the test arrays are small
+  # Start at index i = 1 to length -1
+  # Loop from j = i down to 1
+  # If array[ j ] > array[ j - 1 ]
+  # Swap them
+  # j = j - 1
+  # Else break out of the loop
 end
