0c7663f272f45b0b67ab57462d97239674706ffd
Angela Nguyen <angethuy@gmail.com>
2020-03-21 20:39:52 -0700

    all tests passing

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,20 +1,34 @@
-
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we're going through my_sentence twice but as separate processes
+# Space complexity: O(n), no new structures created or maintained
 def reverse_sentence(my_sentence)
   return my_sentence if my_sentence == nil
 
-  (my_sentence.size/2).times do |x|
-    reverse_range(my_sentence, 0, my_sentence.size-1, x)
+  reverse_range(my_sentence, 0, my_sentence.length-1)
+
+  left = 0
+  right = 0
+
+  while right < my_sentence.length
+    until my_sentence[left] != " " || left+1 == my_sentence.length
+      left += 1
+      right +=1
+    end
+    until my_sentence[right+1] == " " || right+1 == my_sentence.length
+      right+= 1
+    end
+    reverse_range(my_sentence,left,right)
+    left = right+1
+    right = left
+  end
+  
+end
+
+def reverse_range(my_sentence, left, right)
+  ((right-left)/2+1).times do |x|
+    hold_left = my_sentence[left+x]
+    my_sentence[left+x] = my_sentence[right-x]
+    my_sentence[right-x] = hold_left
   end
 end
 
-# TO DO: find every "word" in the sentence and reverse the subarray
-
-
-def reverse_range(my_sentence, left, right, x)
-  hold_left = my_sentence[left+x]
-  my_sentence[left+x] = my_sentence[right-x]
-  my_sentence[right-x] = hold_left
-end
