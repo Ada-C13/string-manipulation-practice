fc534ed9c3b23f55ab653ee760e9f7ddbde9e353
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-24 11:21:12 -0700

    clean up file and add Big 0 Notation

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,18 +1,18 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
+# Time complexity: 0(n) n being the length - a loop based off of n
 # Space complexity: ?
 
 def sort_by_length(my_sentence)
   # verify input. if only one word, return word
   if my_sentence.length == 1
-    return my_sentence
+    return []
   elsif
     # split input sentence into array
     my_sentence_array = my_sentence.split
     # save length of array to varialbe
     my_sentence_array_length = my_sentence_array.length
-    # start loop (I tried 'while' and 'until' but could not get either to work??)
+    # start loop 
       loop do  
         # set swapped to false
         swapped = false
@@ -33,10 +33,3 @@ def sort_by_length(my_sentence)
       return my_sentence_array
   end
 end
-
-
-
-
-
-p sort_by_length("This is a dog")
-# expected output => ["a", "is", "dog", "this"]
