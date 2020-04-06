71a4ae62c1578499c5d567c8cbb02e5a1e9a4a6a
Jessica Stone <jstone4815@gmail.com>
2020-04-05 18:35:26 -0700

    fix code to implement bubble sort properly

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,18 +2,28 @@
 #  sorted by the length of the word.
 # Time complexity: O(n) - loops dependent on length of input
 # Space complexity: O(n) - size of array is dependent on input
+
+# Take a look at the implementations for sorting I gave as an example, as this doesn't actually sort things in general. You can't sort a generic array with only one pass through the array. It looks like you started bubblesort, but didn't put the outer loop in.
+
 def sort_by_length(my_sentence)
   return [] if my_sentence == ""
 
   array = my_sentence.split
-  first_word = array[0]
-  (1..array.length-1).each do |i|
-    current_word = array[i]
-    if current_word.length < first_word.length
-      array[i] = first_word
-      array[i-1] = current_word
+  swapped = true
+  i = 0
+  while i < array.length-1 && swapped
+    j = 0
+    swapped = false
+    while j < array.length-i-1
+      current_word = array[j]
+      if current_word.length > array[j+1].length
+        array[j] = array[j+1]
+        array[j+1] = current_word
+        swapped = true
+      end
+      j += 1
     end
-    first_word = current_word
+    i += 1
   end
 
   return array
